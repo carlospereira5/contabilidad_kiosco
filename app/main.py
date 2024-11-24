edcf25b078e10d38afa3541958fe842fded8c3fd
carlos <carlospereirab55555@gmail.com>
2024-11-24 17:24:14 -0300

    Versión estable inicial con configuración de Docker, base de datos y pruebas iniciales

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+# app/main.py
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
-
 
 def get_application():
     _app = FastAPI(title=settings.PROJECT_NAME)
@@ -15,7 +16,10 @@ def get_application():
         allow_headers=["*"],
     )
 
-    return _app
+    @_app.get("/")
+    async def read_root():
+        return {"message": "Hello World"}
 
+    return _app
 
 app = get_application()
